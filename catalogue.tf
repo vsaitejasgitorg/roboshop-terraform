c2893cc38c9939bf7f25c9af9665ac8c2e960969
vadlasaiteja-git <saitejav236@gmail.com>
2025-02-24 16:24:56 +0530

    Adding Catalogue and Mongodb Servers and Creating DNS records dor all 3 servers

--- catalogue.tf
+++ catalogue.tf
@@ -1,16 +1,16 @@
-resource "aws_instance" "frontend" {
+resource "aws_instance" "catalogue" {
   ami = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
   vpc_security_group_ids = ["sg-0217d4c61f41d9ce7"]
   tags = {
-    Name = "frontend"
+    Name = "catalogue"
   }
 }
 
-resource "aws_route53_record" "frontend" {
-  name    = "frontend-dev"
+resource "aws_route53_record" "catalogue" {
+  name    = "catalogue-dev"
   type    = "A"
   zone_id = "Z0742899AVG59Z59WGO9"
   ttl = 10
-  records = [aws_instance.frontend.private_ip]
+  records = [aws_instance.catalogue.private_ip]
 }
--- mongodb.tf
+++ mongodb.tf
@@ -1,16 +1,16 @@
-resource "aws_instance" "frontend" {
+resource "aws_instance" "mongodb" {
   ami = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
   vpc_security_group_ids = ["sg-0217d4c61f41d9ce7"]
   tags = {
-    Name = "frontend"
+    Name = "mongodb"
   }
 }
 
-resource "aws_route53_record" "frontend" {
-  name    = "frontend-dev"
+resource "aws_route53_record" "mongodb" {
+  name    = "mongodb-dev"
   type    = "A"
   zone_id = "Z0742899AVG59Z59WGO9"
   ttl = 10
-  records = [aws_instance.frontend.private_ip]
+  records = [aws_instance.mongodb.private_ip]
 }
