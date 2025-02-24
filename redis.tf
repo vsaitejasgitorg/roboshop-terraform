b323df196a98a0c98c3d86d7f020786468c281d8
vadlasaiteja-git <saitejav236@gmail.com>
2025-02-24 19:43:48 +0530

    Adding redis.tf

---
+++ redis.tf
@@ -0,0 +1,32 @@
+resource "aws_instance" "redis" {
+  ami = "ami-09c813fb71547fc4f"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0217d4c61f41d9ce7"]
+  tags = {
+    Name = "redis"
+  }
+}
+
+resource "aws_route53_record" "redis" {
+  name    = "redis-dev"
+  type    = "A"
+  zone_id = "Z0742899AVG59Z59WGO9"
+  ttl = 10
+  records = [aws_instance.redis.private_ip]
+}
+
+resource "null_resource" "redis" {
+  provisioner "remote-exec" {
+    connection {
+      type     = "ssh"
+      user     = "ec2-user"
+      password = "DevOps321"
+      host     = aws_instance.redis.private_ip
+    }
+    inline =[
+      "sudo pip3.11 install ansible",
+      "ansible-pull -i localhost, -U https://github.com/vadlasaiteja-git/roboshop-ansible roboshop.yml  -e component_name=redis -e env=dev"
+
+    ]
+  }
+}
