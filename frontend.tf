6a4c7f18f2ba62e5f0e103d3f7806e476b17814a
vadlasaiteja-git <saitejav236@gmail.com>
2025-02-24 16:05:56 +0530

    Adding Security Group in frontend  terraform file

--- frontend.tf
+++ frontend.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "frontend" {
   ami = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0217d4c61f41d9ce7"]
   tags = {
     Name = "frontend"
   }
