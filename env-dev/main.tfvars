fcc81f5ada58f97b20c744ddb19befb86f47aebb
vadlasaiteja-git <saitejav236@gmail.com>
2025-02-26 17:39:30 +0530

    Making Dev and Prod Env directories

---
+++ env-dev/main.tfvars
@@ -0,0 +1,20 @@
+instances = {
+  frontend = {
+    ami_id        = "ami-09c813fb71547fc4f"
+    instance_type = "t3.micro"
+  }
+  catalogue = {
+    ami_id        = "ami-09c813fb71547fc4f"
+    instance_type = "t3.micro"
+  }
+  mongodb = {
+    ami_id        = "ami-09c813fb71547fc4f"
+    instance_type = "t3.small"
+  }
+}
+
+zone_id = "Z0742899AVG59Z59WGO9"
+
+env = "dev"
+
+vpc_security_group_ids = ["sg-0217d4c61f41d9ce7"]
--- variables.tf
+++ variables.tf
@@ -1,30 +1,4 @@
-variable "vpc_security_group_ids" {
-  default = ["sg-0217d4c61f41d9ce7"]
-}
-
-variable "instances" {
-  default = {
-
-    catalogue = {
-      ami_id = "ami-09c813fb71547fc4f"
-      instance_type = "t3.micro"
-
-    }
-    mongodb = {
-      ami_id = "ami-09c813fb71547fc4f"
-      instance_type = "t3.small"
-    }
-    frontend ={
-      ami_id = "ami-09c813fb71547fc4f"
-      instance_type = "t3.micro"
-    }
-  }
-}
-
-variable "zone_id" {
-  default = "Z0742899AVG59Z59WGO9"
-}
-
-variable "env" {
-  default = "dev"
-}
+variable "vpc_security_group_ids" {}
+variable "instances" {}
+variable "zone_id" {}
+variable "env" {}
