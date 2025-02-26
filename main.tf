f1158593362fde4601a5d32fa030368fa868247d
vadlasaiteja-git <saitejav236@gmail.com>
2025-02-26 15:50:49 +0530

    Using for each loop with variable containing keys and values

--- main.tf
+++ main.tf
@@ -6,13 +6,13 @@ resource "aws_instance" "instance" {
   vpc_security_group_ids = var.vpc_security_group_ids
 
   tags = {
-    Name = var.instances[each.key]
+    Name = each.key
   }
 }
 
 resource "aws_route53_record" "record" {
   for_each = var.instances
-  name    = "${var.instances[each.key]}-${var.env}"
+  name    = "${each.key}-${var.env}"
   type    = "A"
   zone_id = var.zone_id
   ttl = 10
