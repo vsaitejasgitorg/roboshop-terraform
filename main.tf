11824c775b4b00eee81635bd2e0e16abf1534d02
vadlasaiteja-git <saitejav236@gmail.com>
2025-03-08 13:07:02 +0530

    Roboshop-Docker

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ module "ec2" {
   vpc_security_group_ids = var.vpc_security_group_ids
   zone_id                = var.zone_id
   vault_token            = var.vault_token
-  ansible_role           = lookup(each.value,"ansible_role",each.key)
+  ansible_role           = lookup(each.value,"ansible_role", each.key)
 
 
 
