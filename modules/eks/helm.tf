533ec38fb3fc2ae99f3c221e7d4b553d73d3ecaf
vadlasaiteja-git <saitejav236@gmail.com>
2025-04-12 17:25:30 +0530

    Setting up Prometheus Helm Charts on Kubernetes Cluster

--- modules/eks/helm.tf
+++ modules/eks/helm.tf
@@ -25,3 +25,10 @@ kubectl patch svc argocd-server -n argocd --patch '{"spec": {"type": "LoadBalanc
 EOF
   }
 }
+
+resource "helm_release" "kube-prometheus-stack" {
+  depends_on = [null_resource.kubeconfig]
+  name       = "kube-prom-stack"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "kube-prometheus-stack"
+}
