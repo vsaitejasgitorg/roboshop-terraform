ff69c580dd6c5c43d33cb24c8590d093d7f4f9bf
vadlasaiteja-git <saitejav236@gmail.com>
2025-04-13 14:14:02 +0530

    Setting up Nginx Ingress Controller for K8s

--- modules/eks/helm.tf
+++ modules/eks/helm.tf
@@ -36,8 +36,7 @@ resource "helm_release" "kube-prometheus-stack" {
 resource "helm_release" "ingress" {
   depends_on = [null_resource.kubeconfig]
   name       = "ingress-nginx"
-  repository = "https://kubernetes.github.io/ingress-nginx"
-  chart      = "ingress-nginx"
+  chart = "oci://ghcr.io/nginxinc/charts/nginx-ingress"
 
   values = [
     file("${path.module}/helm-config/ingress.yml")
