dbefb3bf3d51c6c323dd6b838f7bd958448ee16f
vadlasaiteja-git <saitejav236@gmail.com>
2025-04-15 00:01:17 +0530

    Setting up Cert Manager with Helm

--- modules/eks/helm.tf
+++ modules/eks/helm.tf
@@ -54,10 +54,9 @@ resource "helm_release" "cert-manager" {
   namespace = "cert-manager"
   create_namespace = true
 
-  set = [
-    {
+  set {
       name  = "crds.enabled"
       value = "true"
     }
-  ]
+
 }
