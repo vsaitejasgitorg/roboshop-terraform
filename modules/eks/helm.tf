9c5055b14360c671aa7a1d3f47a307b447e7982f
vadlasaiteja-git <saitejav236@gmail.com>
2025-04-15 00:13:08 +0530

    Setting up Cert Manager with Helm

--- modules/eks/helm.tf
+++ modules/eks/helm.tf
@@ -60,3 +60,12 @@ resource "helm_release" "cert-manager" {
     }
 
 }
+
+
+resource "null_resource" "cert-manager-cluster-issuer" {
+  depends_on = [null_resource.kubeconfig, helm_release.cert-manager]
+
+  provisioner "local-exec" {
+    command = "kubectl apply -f ${path.module}/helm-config/cluster-issuer.yml"
+  }
+}
