c4124e1ea9daebd62c306077df9eda5d22c9de62
vadlasaiteja-git <saitejav236@gmail.com>
2025-04-21 11:20:09 +0530

    Adding Grafana Dashboards using Terraform

--- modules/eks/grafana.tf
+++ modules/eks/grafana.tf
@@ -3,7 +3,7 @@ resource "grafana_folder" "ec2-nodes" {
   uid   = "ec2-nodes"
 }
 resource "grafana_dashboard" "node-exporter" {
-  folder = "Dashboards"
+  folder = grafana_folder.ec2-nodes.uid
   config_json = jsonencode({
     "title" : "Node Exporter Full",
     "dashboard_id" : "12486"
